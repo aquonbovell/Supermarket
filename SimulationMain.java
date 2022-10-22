82dde38d25d5eb608afe0e0fad1bd0fbc82a55bb
Aquon Bovell <aquon_bovell@outlook.com>
2022-10-22 18:47:48 -0400

    added comments

--- SimulationMain.java
+++ SimulationMain.java
@@ -1,21 +1,25 @@
 public class SimulationMain {
-  public static void main(String[] args) {
-    for (int i = 0; i < args.length; ++i) {
-      if (args[i].equals("iter")) {
-        // since an array is a reference if we used "==" it would be false as memory
-        // locations are different using the equals method solves that
-
-        if (i < args.length - 1) {
-          i++;
-          try {
-            int iter = Integer.parseInt(args[i]);
-            // set total simulation iterations
-            System.out.print(iter);
-          } catch (NumberFormatException e) {
-            System.out.println("Invalid iter command, total number of iterations is missing");
-          }
-        }
-      }
-    }
-  }
+    public static void main(String args[]){
+		for(int i = 0; i < args.length; i++){
+			if(args[i].equals("iter")) {
+				if(i < args.length - 1){
+					i++;
+					try{
+						int iter = Integer.parseInt(args[i]);
+            System.out.println(iter);
+						Supermarket market = new Supermarket();
+            // This is where all the functionality go
+						// set total simulation iterations
+            // Display the menu
+            // Call this method to run the simulastor; market.run();
+					}
+					catch(NumberFormatException e) {
+						System.out.println("Invalid iter command, total number of iterations is missing");
+					}
+					
+				}
+			}
+		}
+		
+	}
 }
--- Supermarket.java
+++ Supermarket.java
@@ -1,3 +1,16 @@
 public class Supermarket{
-  
+  private int totalIterations;
+	public final int DEFAULT_TOTAL_ITERATIONS = 50;
+	
+	public Supermarket()
+	{
+		totalIterations = DEFAULT_TOTAL_ITERATIONS;
+	}
+	
+	public void run()
+	{
+		for(int i = 0; i < totalIterations; i++){
+			// perform simulation actions.
+		}
+	}
 }
