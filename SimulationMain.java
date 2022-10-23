b56cd9c44f82fff72cedb47c165653485feae488
Aquon Bovell <aquon_bovell@outlook.com>
2022-10-23 18:14:43 -0400

    intial commit

--- SimulationMain.java
+++ SimulationMain.java
@@ -1,17 +1,16 @@
-public class SimulationMain {
-    public static void main(String args[]){
+public class SimulationMain
+{
+	public static void main(String args[]){
+		Supermarket market = new Supermarket();
+		
 		for(int i = 0; i < args.length; i++){
 			if(args[i].equals("iter")) {
 				if(i < args.length - 1){
 					i++;
 					try{
 						int iter = Integer.parseInt(args[i]);
-            System.out.println(iter);
-						Supermarket market = new Supermarket();
-            // This is where all the functionality go
+						System.out.println(iter);
 						// set total simulation iterations
-            // Display the menu
-            // Call this method to run the simulastor; market.run();
 					}
 					catch(NumberFormatException e) {
 						System.out.println("Invalid iter command, total number of iterations is missing");
@@ -21,5 +20,7 @@ public class SimulationMain {
 			}
 		}
 		
+		// Display the menu
+		// Call this method to run the simulastor; market.run();
 	}
 }
